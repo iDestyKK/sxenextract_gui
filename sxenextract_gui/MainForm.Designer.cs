ae28dc5ac59b6a5958e2d4baa227535d5b28185a
iDestyKK <idesty@derpg.net>
2014-05-19 20:20:54 -0400

    Initial Commit

---
+++ sxenextract_gui/MainForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace sxenextract_gui
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem5 = new System.Windows.Forms.MenuItem();
+            this.menuItem_open = new System.Windows.Forms.MenuItem();
+            this.menuItem7 = new System.Windows.Forms.MenuItem();
+            this.menuItem_Exit = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.menuItem_extractAll = new System.Windows.Forms.MenuItem();
+            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItem9 = new System.Windows.Forms.MenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView_explorer = new System.Windows.Forms.TreeView();
+            this.listView_explorer = new System.Windows.Forms.ListView();
+            this.openFileDialog_sxen = new System.Windows.Forms.OpenFileDialog();
+            this.columnFilename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1,
+            this.menuItem2,
+            this.menuItem3});
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 0;
+            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem5,
+            this.menuItem_open,
+            this.menuItem7,
+            this.menuItem_Exit});
+            this.menuItem1.Text = "&File";
+            // 
+            // menuItem5
+            // 
+            this.menuItem5.Index = 0;
+            this.menuItem5.Text = "&New";
+            // 
+            // menuItem_open
+            // 
+            this.menuItem_open.Index = 1;
+            this.menuItem_open.Text = "&Open";
+            this.menuItem_open.Click += new System.EventHandler(this.menuItem_open_Click);
+            // 
+            // menuItem7
+            // 
+            this.menuItem7.Index = 2;
+            this.menuItem7.Text = "-";
+            // 
+            // menuItem_Exit
+            // 
+            this.menuItem_Exit.Index = 3;
+            this.menuItem_Exit.Text = "&Exit";
+            this.menuItem_Exit.Click += new System.EventHandler(this.menuItem_Exit_Click);
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Index = 1;
+            this.menuItem2.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem_extractAll});
+            this.menuItem2.Text = "&Options";
+            // 
+            // menuItem_extractAll
+            // 
+            this.menuItem_extractAll.Index = 0;
+            this.menuItem_extractAll.Text = "E&xtract All";
+            this.menuItem_extractAll.Click += new System.EventHandler(this.menuItem_extractAll_Click);
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.Index = 2;
+            this.menuItem3.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem9});
+            this.menuItem3.Text = "&Help";
+            // 
+            // menuItem9
+            // 
+            this.menuItem9.Index = 0;
+            this.menuItem9.Text = "&About";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView_explorer);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView_explorer);
+            this.splitContainer1.Size = new System.Drawing.Size(718, 357);
+            this.splitContainer1.SplitterDistance = 227;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView_explorer
+            // 
+            this.treeView_explorer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView_explorer.Location = new System.Drawing.Point(0, 0);
+            this.treeView_explorer.Name = "treeView_explorer";
+            this.treeView_explorer.Size = new System.Drawing.Size(227, 357);
+            this.treeView_explorer.TabIndex = 0;
+            // 
+            // listView_explorer
+            // 
+            this.listView_explorer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnFilename,
+            this.columnType,
+            this.columnSize});
+            this.listView_explorer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView_explorer.Location = new System.Drawing.Point(0, 0);
+            this.listView_explorer.Name = "listView_explorer";
+            this.listView_explorer.Size = new System.Drawing.Size(487, 357);
+            this.listView_explorer.TabIndex = 0;
+            this.listView_explorer.UseCompatibleStateImageBehavior = false;
+            this.listView_explorer.View = System.Windows.Forms.View.Details;
+            // 
+            // openFileDialog_sxen
+            // 
+            this.openFileDialog_sxen.DefaultExt = "sxen";
+            this.openFileDialog_sxen.Filter = "DERPG SXEN Archives (*.sxen)|*.sxen";
+            this.openFileDialog_sxen.Title = "Select a SXEN archive to open";
+            // 
+            // columnFilename
+            // 
+            this.columnFilename.Text = "Name";
+            this.columnFilename.Width = 240;
+            // 
+            // columnSize
+            // 
+            this.columnSize.Text = "Size";
+            this.columnSize.Width = 120;
+            // 
+            // columnType
+            // 
+            this.columnType.Text = "Type";
+            this.columnType.Width = 120;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 357);
+            this.Controls.Add(this.splitContainer1);
+            this.Menu = this.mainMenu1;
+            this.Name = "MainForm";
+            this.Text = "sxenextract_gui by iDestyKK";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem5;
+        private System.Windows.Forms.MenuItem menuItem_open;
+        private System.Windows.Forms.MenuItem menuItem7;
+        private System.Windows.Forms.MenuItem menuItem_Exit;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.MenuItem menuItem_extractAll;
+        private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.MenuItem menuItem9;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView_explorer;
+        private System.Windows.Forms.ListView listView_explorer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog_sxen;
+        private System.Windows.Forms.ColumnHeader columnFilename;
+        private System.Windows.Forms.ColumnHeader columnType;
+        private System.Windows.Forms.ColumnHeader columnSize;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ sxenextract_gui/MainForm.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sxenextract_gui
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            SetWindowTheme(listView_explorer.Handle, "explorer", null);
+            SetWindowTheme(treeView_explorer.Handle, "explorer", null);
+        }
+
+        [DllImport("uxtheme.dll", CharSet = CharSet.Unicode)]
+        public static extern int SetWindowTheme(IntPtr hWnd, String pszSubAppName, String pszSubIdList);
+
+        static string song_name = "";
+        static string song_author = "";
+        static string song_year = "";
+        static string song_length = "";
+        static string song_genre = "";
+        static string song_album = "";
+        static string song_comment = "";
+        static string gfile = "";
+
+        public class Files
+        {
+            public Files(string a, int b, long c, int d)
+            {
+                filename = a;
+                size = b;
+                pos = c;
+                type = d;
+            }
+            public Files()
+            {
+                filename = "";
+                size = 0;
+                pos = 0;
+                type = 0;
+            }
+            public string filename;
+            public int size;
+            public long pos;
+            public int type;
+        }
+
+        static List<Files> filedata = new List<Files>();
+        static List<string> groups = new List<string>();
+
+        void OpenSXENArchive(string filename)
+        {
+            FileStream fp = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            //EXTREMELY UNORTHODOX HEADER CHECK
+            if (fp.ReadByte() == 0x44 &&
+                fp.ReadByte() == 0x45 &&
+                fp.ReadByte() == 0x52 &&
+                fp.ReadByte() == 0x50 &&
+                fp.ReadByte() == 0x47 &&
+                fp.ReadByte() == 0x5F &&
+                fp.ReadByte() == 0x53 &&
+                fp.ReadByte() == 0x4F &&
+                fp.ReadByte() == 0x4E &&
+                fp.ReadByte() == 0x47 &&
+                fp.ReadByte() == 0x50 &&
+                fp.ReadByte() == 0x41 &&
+                fp.ReadByte() == 0x4B) //DERPG_SONGPAK
+            {
+                gfile = filename;
+                int version = (fp.ReadByte() << 8) + fp.ReadByte();
+                switch (version)
+                {
+                    case 1: openSXEN_v01(fp); break;
+                    case 2: openSXEN_v02(fp); break;
+                }
+                fp.Close(); //We are done here.
+
+                //Update the ListView.
+                listView_explorer.Items.Clear();
+                treeView_explorer.Nodes.Clear();
+                ListViewGroup[] group = new ListViewGroup[groups.Count()];
+                treeView_explorer.Nodes.Add("main", Path.GetFileName(filename));
+
+                //Categories go first.
+                for (int i = 0; i < groups.Count(); i++)
+                {
+                    //Add the group to the ListView
+                    group[i] = new ListViewGroup(groups[i], groups[i]);
+                    listView_explorer.Groups.Add(group[i]);
+
+                    //Add the group to the TreeView
+                    TreeNode gnode = new TreeNode(groups[i]);
+                    treeView_explorer.Nodes["main"].Nodes.Add(gnode);
+                }
+
+                //Now it is the file's turn.
+                for (int i = 0; i < filedata.Count(); i++)
+                {
+                    string p = Path.GetExtension(filedata[i].filename);
+                    ListViewItem item = new ListViewItem(filedata[i].filename);
+                    item.SubItems.Add(p.Substring(1, p.Length - 1).ToUpper() + " File");
+                    item.SubItems.Add(ByteSimplify(filedata[i].size));
+                    item.Group = group[filedata[i].type];
+                    listView_explorer.Items.Add(item);
+
+                    //listView_explorer.Items.Add(filedata[i].filename).SubItems.AddRange(new string[] { p.Substring(1, p.Length - 1).ToUpper() + " File", ByteSimplify(filedata[i].size) });
+                }
+                treeView_explorer.ExpandAll();
+            }
+        }
+
+        string ByteSimplify(int size)
+        {
+            string[] bytetypes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" }; //To be outdated in 2160年
+            int count = 0;
+            while (size > 1024)
+            {
+                size /= 1024;
+                count += 1;
+            }
+            return size.ToString() + " " + bytetypes[count];
+        }
+        
+        void openSXEN_v01(FileStream fp)
+        {
+            while (fp.Position < fp.Length)
+            {
+                switch (fp.ReadByte())
+                {
+                    case 0x00:
+                        //File
+                        filedata.Add(new Files(readString(fp), VLQ_to_int(fp), fp.Position, groups.Count - 1));
+                        fp.Seek(filedata[filedata.Count - 1].size, SeekOrigin.Current);
+                        break;
+                    case 0xFF:
+                        //Marker
+                        groups.Add(readString(fp));
+                        break;
+                }
+            }
+        }
+
+        int VLQ_to_int(FileStream fp)
+        {
+            int total = 0;
+            string bits = "";
+            int b;
+            do {
+                b = fp.ReadByte();
+                string seq = "";
+                for (int i = 0; i < 7; i++)
+                    seq = Convert.ToInt32((b & (1 << i)) != 0).ToString() + seq;
+                bits = bits + seq;
+            } while ((b & (1 << 7)) != 0);
+
+            for (int i = 0; i < bits.Length; i++)
+                total += Convert.ToInt32(Math.Pow((double)2, Convert.ToDouble(bits.Length - (i + 1))) * Convert.ToDouble(bits[i] != '0'));
+
+            return total;
+        }
+
+        void openSXEN_v02(FileStream fp)
+        {
+            //Read Strings
+            song_name    = readString(fp);
+            song_author  = readString(fp);
+            song_year    = readString(fp);
+            song_length  = readString(fp);
+            song_genre   = readString(fp);
+            song_album   = readString(fp);
+            song_comment = readString(fp);
+
+            //The rest is the same as the previous version.
+            openSXEN_v01(fp);
+        }
+
+        string readString(FileStream fp)
+        {
+            string str = "";
+            int length = fp.ReadByte();
+            for (int i = 0; i < length; i++)
+                str += Convert.ToChar(fp.ReadByte());
+            return str;
+        }
+
+        private void menuItem_open_Click(object sender, EventArgs e)
+        {
+            DialogResult fs = openFileDialog_sxen.ShowDialog();
+            if (fs == DialogResult.OK)
+                if (File.Exists(openFileDialog_sxen.FileName))
+                    OpenSXENArchive(openFileDialog_sxen.FileName);
+        }
+
+        private void menuItem_extractAll_Click(object sender, EventArgs e)
+        {
+            DialogResult fs = folderBrowserDialog.ShowDialog();
+            if (fs == DialogResult.OK)
+            {
+                string path = folderBrowserDialog.SelectedPath;
+                FileStream fp = new FileStream(gfile, FileMode.Open, FileAccess.Read);
+                for (int i = 0; i < filedata.Count(); i++)
+                {
+                    //BEGIN WRITING FILE.
+                    fp.Seek(filedata[i].pos, SeekOrigin.Begin);
+                    FileStream np = new FileStream(path + "\\" + filedata[i].filename, FileMode.OpenOrCreate, FileAccess.Write);
+                    for (int a = 0; a < filedata[i].size; a++)
+                        np.WriteByte(Convert.ToByte(fp.ReadByte()));
+                    np.Close();
+                }
+                fp.Close();
+            }
+        }
+
+        private void menuItem_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
